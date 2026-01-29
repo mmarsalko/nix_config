fb3ac1ce5870cb3a46ee48e02d5d3eba7f5e2725
Shaffle <mmarsalko+github@protonmail.com>
2026-01-29 15:24:07 -0800

    rm flatpak from server

--- hosts/matt-htpc/configuration.nix
+++ hosts/matt-htpc/configuration.nix
@@ -9,7 +9,6 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../modules/nixos/general.nix
-      ../../modules/nixos/flatpak.nix
     ];
 
   # Flakeys
