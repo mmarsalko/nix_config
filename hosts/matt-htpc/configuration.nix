c00fe024dd8d3eb0d54592909dfbe28b0e8ea72a
Shaffle <mmarsalko+github@protonmail.com>
2026-01-29 16:36:10 -0800

    Fix?

--- hosts/matt-htpc/configuration.nix
+++ hosts/matt-htpc/configuration.nix
@@ -46,10 +46,7 @@
 
   # Pulled from Nixos fish wiki. Launches fish from bash when in an interactive terminal
   # Recommended because fish being used as login shell is bad for POSIX reasons
-  programs.starship = {
-    enable = true;
-    enableInteractive = true;
-  };
+  programs.starship.enable = true;
   programs.bash = {
   interactiveShellInit = ''
     if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
